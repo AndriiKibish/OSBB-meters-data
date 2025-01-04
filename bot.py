eb47dcbfe68a2116f2b0d02f45487d3df378fb0a
Andrii K <andyki@bigmir.net>
2025-01-04 09:41:45 +0200

    Fourth commit

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from flask import Flask
 from aiogram.utils.executor import start_webhook
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = os.getenv(TELEGRAM_TOKEN)
 
 
 # Определение состояний диалога
@@ -91,6 +91,3 @@ if __name__ == '__main__':
     # Запуск асинхронного цикла с Flask с использованием asyncio.run()
     asyncio.run(on_start())  # Запуск Telegram бота
     run_app()  # Запуск Flask
-
-
-
